bfa831ac1fb3cba927a281cb8cdf341ee123d9ae
Ilyas Zhanaev <izhanaev@mail.ru>
2024-07-30 17:08:20 +0300

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Ильяс Жанаев on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
